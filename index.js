3118f0478734c342c33c3c35de6a00f61e5b2486
Manase Kimutai <mernasseh@gmail.com>
2025-03-19 22:34:51 +0300

    initiate commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const paragraphElement = document.getElementById('text');
+
+    if (paragraphElement) {
+        paragraphElement.textContent = "This is really cool!";
+    }
+  });
+
+
+
